c05e4fa1ebd5bbf2c368431d62b04fa3980253c2
Devarshee <devarshee.sandilya@donaldson.com>
2026-05-03 22:10:53 +0530

    edited config file

--- config.py
+++ config.py
@@ -4,6 +4,11 @@ from dotenv import load_dotenv
 # load .env only for local development
 load_dotenv()
 
+
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+if not OPENAI_API_KEY:
+    raise ValueError("OPENAI_API_KEY is missing. Check .env or environment variables.")
+
 CHROMA_PATH = os.getenv("CHROMA_PATH", "chroma")
 DATA_PATH = os.getenv("DATA_PATH", "data")
