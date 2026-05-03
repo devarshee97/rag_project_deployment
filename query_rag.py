395d05baada01268863d46413e681d544559c9a6
Devarshee <devarshee.sandilya@donaldson.com>
2026-05-03 22:34:59 +0530

    imported chroma

--- query_rag.py
+++ query_rag.py
@@ -1,7 +1,7 @@
 from langchain_community.document_loaders import DirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
-#from langchain_community.vectorstores import Chroma
+from langchain_community.vectorstores import Chroma
 import openai 
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain_core.documents import Document
